0bdccaab2a5669c970e36d186677225373b944c7
miro-oss <roxkstar@Roxkstar-2.local>
2025-10-11 00:33:22 +0900

    mission/#04

---
+++ src/main/java/com/example/umc9th/domain/member/entity/Food.java
@@ -0,0 +1,26 @@
+package com.example.umc9th.domain.member.entity;
+
+import com.example.umc9th.domain.member.entity.mapping.MemberFood;
+import com.example.umc9th.domain.member.enums.FoodName;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@Getter
+@Table(name = "food")
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "food_id")
+    private Long id;
+
+    @Column(name = "name")
+    @Enumerated(EnumType.STRING)
+    private FoodName name = FoodName.NONE;
+}
