ba7343bf18f946ad0d5ea9c8c30eb340152c9ec5
sunny <anna7789@naver.com>
2025-10-12 22:43:31 +0900

    mission/#4

---
+++ src/main/java/com/example/umc9th/domain/store/entity/Location.java
@@ -0,0 +1,21 @@
+package com.example.umc9th.domain.store.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "location")
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "location_id")
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name; // 지역명 (예: 강남구)
+}
