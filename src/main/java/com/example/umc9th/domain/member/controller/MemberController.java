1af8d824c31fb8221cbb29bf5caceb41ffe20fd4
miro-oss <tldms822@naver.com>
2025-11-07 03:10:14 +0900

    Update MemberController.java

--- src/main/java/com/example/umc9th/domain/member/controller/MemberController.java
+++ src/main/java/com/example/umc9th/domain/member/controller/MemberController.java
@@ -1,7 +1,6 @@
 package com.example.umc9th.domain.member.controller;
 
 import com.example.umc9th.domain.member.dto.MypageResponseDto;
-import com.example.umc9th.domain.member.dto.MypageResponseDto;
 import com.example.umc9th.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
