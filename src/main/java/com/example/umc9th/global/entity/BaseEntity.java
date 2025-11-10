66c61ae5b81db9bd27cae16bf3cc9a30b8e921c4
정준영 <wnsdud1158@naver.com>
2025-11-11 00:27:30 +0900

    mission/#7

--- src/main/java/com/example/umc9th/global/entity/BaseEntity.java
+++ src/main/java/com/example/umc9th/global/entity/BaseEntity.java
@@ -1,11 +1,13 @@
 package com.example.umc9th.global.entity;
 
+import com.querydsl.core.annotations.QuerySupertype;
 import jakarta.persistence.Column;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
+@QuerySupertype
 public class BaseEntity {
 
     @CreatedDate
