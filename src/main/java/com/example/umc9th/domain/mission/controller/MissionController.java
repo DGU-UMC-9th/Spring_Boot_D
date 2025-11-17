9a920603ee1d674b2493f901df645c2045b55f70
sunny <anna7789@naver.com>
2025-11-17 16:43:44 +0900

    mission/#7: MissionController 응답통일

--- src/main/java/com/example/umc9th/domain/mission/controller/MissionController.java
+++ src/main/java/com/example/umc9th/domain/mission/controller/MissionController.java
@@ -2,6 +2,8 @@ package com.example.umc9th.domain.mission.controller;
 
 import com.example.umc9th.domain.mission.dto.HomeMissionItemDto;
 import com.example.umc9th.domain.mission.service.HomeMissionService;
+import com.example.umc9th.global.apiPayload.ApiResponse;
+import com.example.umc9th.global.apiPayload.code.GeneralSuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,12 +19,18 @@ public class MissionController {
 
     private final HomeMissionService homeMissionService;
 
-    @GetMapping("/home")
-    public Page<HomeMissionItemDto> getHomeMissions(
+    @GetMapping //http://localhost:8080/api/missions/home?memberId=1&locationId=1
+    public ApiResponse<Page<HomeMissionItemDto>> getHomeMissions(
             @RequestParam Long memberId,
             @RequestParam Long locationId,
             Pageable pageable
     ) {
-        return homeMissionService.getHomeMissions(memberId, locationId, pageable);
+        Page<HomeMissionItemDto> missions =
+                homeMissionService.getHomeMissions(memberId, locationId, pageable);
+
+        return ApiResponse.onSuccess(
+                GeneralSuccessCode.OK,
+                missions
+        );
     }
 }
