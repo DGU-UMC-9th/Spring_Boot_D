a027f03553b28ff551c0834e2c0c4059e61d144b
sunny <anna7789@naver.com>
2025-11-17 16:47:44 +0900

    mission/#7: 요청 파라미터 누락 시 로깅/BadRequest 응답 추가

--- src/main/java/com/example/umc9th/global/apiPayload/handler/GeneralExceptionAdvice.java
+++ src/main/java/com/example/umc9th/global/apiPayload/handler/GeneralExceptionAdvice.java
@@ -4,10 +4,14 @@ import com.example.umc9th.global.apiPayload.ApiResponse;
 import com.example.umc9th.global.apiPayload.code.BaseErrorCode;
 import com.example.umc9th.global.apiPayload.code.GeneralErrorCode;
 import com.example.umc9th.global.apiPayload.exception.GeneralException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GeneralExceptionAdvice {
 
@@ -23,6 +27,17 @@ public class GeneralExceptionAdvice {
                                 null
                         )
                 );
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ApiResponse<?>> handleMissingServletRequestParameterException(
+            MissingServletRequestParameterException e
+    ) {
+        log.warn("[MissingServletRequestParameterException] {}", e.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.onFailure(GeneralErrorCode.BAD_REQUEST));
     }
 
     // 그 외의 정의되지 않은 모든 예외 처리
